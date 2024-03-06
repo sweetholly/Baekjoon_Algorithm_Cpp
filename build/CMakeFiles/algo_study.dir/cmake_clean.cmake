8ed900887a456d7cacb365e4563fc6e73c057408
sweetholly <80771455+sweetholly@users.noreply.github.com>
2024-03-06 07:06:13 +0000

    CMake Set

---
+++ build/CMakeFiles/algo_study.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/algo_study.dir/main.cpp.o"
+  "algo_study"
+  "algo_study.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/algo_study.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/algo_study.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for algo_study.
+# This may be replaced when dependencies are built.
