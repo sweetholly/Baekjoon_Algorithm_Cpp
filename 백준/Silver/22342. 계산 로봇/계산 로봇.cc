fb7b6fd1453544c944c84c2a2fc9d4c6c772bd56
sweetholly <80771455+sweetholly@users.noreply.github.com>
2026-03-10 00:08:18 +0900

    [Silver I] Title: 계산 로봇, Time: 192 ms, Memory: 33304 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/22342. 계산 로봇/계산 로봇.cc
+++ 백준/Silver/22342. 계산 로봇/계산 로봇.cc
@@ -1,52 +1,45 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <iostream>
 #include <algorithm>
 #include <string>
-
 using namespace std;
-int weight[2010][2010];
-int output[2010][2010];
-int MAXVALUE = 0;
+
+int weight[2001][2001];
+int out[2001][2001]; // 출력 값
+
 int main()
 {
     int M, N;
     cin >> M >> N;
-    
-    for(int i = 0; i <=N; i++){
-        weight[0][i] = 0;  
-        weight[M+1][i] = 0;
-        output[0][i] = 0;  
-        output[M+1][i] = 0;
-    }
-    for(int i = 1; i <=M; i ++){
-        weight[i][0] = 0;
-    }
-    
+
     for(int i = 1; i <= M; i++){
-        string input = "";
+        string input;
         cin >> input;
-        for(int j =1; j <= N; j++){
-            int w = input[j-1] - '0';
-            weight[i][j] = w;
-        }
+        for(int j = 1; j <= N; j++)
+            weight[i][j] = input[j-1] - '0';
+    }
+
+    int ans = 0;
+
+    // 첫 열: 저장 값 = 0, 출력 값 = weight
+    for(int i = 1; i <= M; i++) {
+        out[i][1] = weight[i][1];
     }
     
-    
-    for(int x = 0; x <= M; x++){
-        for(int y =0; y < N; y++){
-            output[x][y] = max({output[x][y-1], output[x-1][y-1], output[x+1][y-1]});
-            output[x][y] += weight[x][y];
-            MAXVALUE = max(MAXVALUE, output[x][y]);
-            //cout<<x<<","<<y<<"weight : "<<weight[x][y]<<",, output : "<<output[x][y]<<endl;
+    // 둘째 열부터
+    for(int j = 2; j <= N; j++){
+        for(int i = 1; i <= M; i++){
+            int store = out[i][j-1];
+            if(i > 1) {
+                store = max(store, out[i-1][j-1]);
+            }
+            if(i < M) {
+                store = max(store, out[i+1][j-1]);
+            }
+            ans = max(ans, store);      
+            out[i][j] = store + weight[i][j];
         }
     }
-    cout<<MAXVALUE;
+
+    cout << ans << endl;
     return 0;
 }
