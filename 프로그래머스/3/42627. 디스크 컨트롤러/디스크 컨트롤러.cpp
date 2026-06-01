9e19e7d9adbe8610d1a19f4803b01941c2ea519f
sweetholly <80771455+sweetholly@users.noreply.github.com>
2026-06-01 15:27:35 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.07 ms, Memory: 3.94 MB -BaekjoonHub

---
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
@@ -0,0 +1,50 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+/*
+    시간 > 요청 > 번호 작은 것
+    비선점형
+    일종의 비선점SJF?
+*/
+int solution(vector<vector<int>> jobs) {
+    int answer = 0;
+    int currentTime = 0;
+    int totalTurnaroundTime = 0;
+    int completed = 0;
+    int jobIndex = 0;
+    int n = jobs.size();
+    
+    sort(jobs.begin(), jobs.end());
+    
+    priority_queue
+        <
+            pair<int, int>,
+            vector<pair<int, int>>,
+            greater<pair<int, int>>
+        > 
+        workQueue;
+    
+    while(completed < n){
+        //현재 시간과, jobs 벡터 맨 앞의 일의 입력 시간 비교
+        while (jobIndex < n && jobs[jobIndex][0] <= currentTime)         {
+            workQueue.push({jobs[jobIndex][1], jobs[jobIndex][0]});
+            jobIndex++;
+        }
+        if (workQueue.empty()) {
+            currentTime = jobs[jobIndex][0];
+            continue;
+        }
+        auto [work_time, request_time] = workQueue.top();
+        workQueue.pop();
+
+        currentTime += work_time;
+        totalTurnaroundTime += currentTime - request_time;
+        completed++;
+    }
+    answer = totalTurnaroundTime / n;
+    return answer;
+}
